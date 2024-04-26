34ed3a8411f0a28bc95f73778514ce90308c937b
Abbos <savrjonov04@gmail.com>
2024-04-26 16:44:01 +0500

    start

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Map from "./Map/map"
+import Header from "./Header/header"
+import Download from "./Download/download"
+import MakeOwner from "./MakeOwner/makeOwner"
+export { Map , Header , Download , MakeOwner}
